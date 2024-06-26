cfe91cb6f672fac0132fca43bc18ce6338ec6848
『itzzzyashu™』 <83384892+itzzzyashu@users.noreply.github.com>
2024-06-25 18:48:04 -0700

    Add files via upload

---
+++ sh/ultroid/termux/pip-installs.sh
@@ -0,0 +1,8 @@
+pip install -r ./requirements.txt
+pip install -r ./resources/startup/optional-requirements.txt
+pip install telegraph google-trans-new playwright redis safety pillow pytz qrcode youtube-search-python
+pip install -U telethon
+pip install tgcalls==3.0.0.dev6
+pip install pytgcalls==2.1.0 --no-deps
+playwright install-deps
+playwright install
