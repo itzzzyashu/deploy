41b35b3dd0c3b725e7d25bead7774a2ccf00c9bb
『itzzzyashu™』 <83384892+itzzzyashu@users.noreply.github.com>
2024-06-26 07:25:59 +0530

    Update termux-setup.sh

--- sh/ultroid/termux/termux-setup.sh
+++ sh/ultroid/termux/termux-setup.sh
@@ -1,6 +1,13 @@
-sudo apt update && sudo apt upgrade -y
-sudo apt install curl -y
+echo "Installing Required Packages in 3 seconds..."
+sleep 3
+apt update && apt upgrade -y
+apt install curl -y
 pkg install git -y
+
+echo "Setting up your Termux to install Ubuntu"
 bash -c "$(curl -fsSL https://gist.githubusercontent.com/rooted-cyber/495de5da4c5dd133f64b2319c61de845/raw/Termux-setup.sh)"
 sh -c "$(curl -fsSl https://gist.githubusercontent.com/rooted-cyber/f2fe4077f65c2a8a8447c24d3af96c13)"
+
+echo "Activating Zsh Shell in 3 seconds..."
+sleep 3
 zsh
