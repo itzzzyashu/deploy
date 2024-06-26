9123e636d45e5922c761e600a81c8399d7000e90
『itzzzyashu™』 <83384892+itzzzyashu@users.noreply.github.com>
2024-06-26 07:51:12 +0530

    Update ease-setup.sh

--- sh/ultroid/termux/ease-setup.sh
+++ sh/ultroid/termux/ease-setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 curl -o ./os -L https://raw.githubusercontent.com/itzzzyashu/deploy/main/sh/ultroid/termux/easy/os
+chmod +x ./os
 
 mkdir -p ./ubuntu-fs/root/
 curl -o ./ubuntu-fs/root/play -L https://raw.githubusercontent.com/itzzzyashu/deploy/main/sh/ultroid/termux/easy/play
+chmod +x ./ubuntu-fs/root/play
