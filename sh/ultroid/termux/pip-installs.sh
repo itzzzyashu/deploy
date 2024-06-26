0b975e46fb1c7e8679bf09b83cff929d91c65503
『itzzzyashu™』 <83384892+itzzzyashu@users.noreply.github.com>
2024-06-26 07:32:04 +0530

    Update pip-installs.sh

--- sh/ultroid/termux/pip-installs.sh
+++ sh/ultroid/termux/pip-installs.sh
@@ -1,3 +1,5 @@
+echo "Have patience until we install all the pypi packages required for Ultroid to work, Starting in 3 seconds"
+sleep 3
 pip install -r ./requirements.txt
 pip install -r ./resources/startup/optional-requirements.txt
 pip install telegraph google-trans-new playwright redis safety pillow pytz qrcode youtube-search-python
@@ -6,3 +8,5 @@ pip install tgcalls==3.0.0.dev6
 pip install pytgcalls==2.1.0 --no-deps
 playwright install-deps
 playwright install
+
+echo "Requirements as installed successfully"
