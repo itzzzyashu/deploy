0e891d44e88e3f379285ac0e16710a6cd18426f0
『itzzzyashu™』 <83384892+itzzzyashu@users.noreply.github.com>
2024-06-26 07:23:33 +0530

    Update ultroid-setup.sh

--- sh/ultroid/termux/ultroid-setup.sh
+++ sh/ultroid/termux/ultroid-setup.sh
@@ -1,5 +1,12 @@
-git clone https://github.com/TeamUltroid/Ultroid
+echo "Downloading Official Repository: TeamUltroid@github.com/Ultroid"
+git clone https://github.com/TeamUltroid/Ultroid.git
 cd Ultroid
+
+echo "Installing Virtual Environment"
 pip install virtualenv
+
+echo "Setting up Virtual Environment"
 virtualenv venv
+
+echo "Activating Virtual Environment"
 . ./venv/bin/activate
